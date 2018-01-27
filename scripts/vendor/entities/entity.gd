4dcaefb55c67d3c7bdd8db9cb5217f9568eab306
CzlowiekImadlo <quak@tlen.pl>
2018-01-27 12:23:41 +0100

    Added vendor entities

---
+++ scripts/vendor/entities/entity.gd
@@ -0,0 +1,42 @@
+
+var board
+
+var avatar
+var is_processing = false
+var free_avatar = true
+var initial_position = Vector3(0, 0, 0)
+
+func _init(board):
+    self.board = board
+
+func get_pos():
+    return self.avatar.get_pos()
+
+func set_pos(position):
+    self.avatar.set_pos(position)
+
+func spawn(position):
+    self.attach()
+    self.initial_position = position
+    self.avatar.set_pos(self.initial_position)
+
+func despawn():
+    self.is_processing = false
+    self.detach()
+    if self.free_avatar:
+        self.avatar.queue_free()
+
+func attach():
+    self.is_processing = true
+    self.board.attach_object(self.avatar)
+
+func detach():
+    self.is_processing = false
+    self.board.detach_object(self.avatar)
+
+func calculate_distance_to_object(object):
+    return self.calculate_distance(object.get_pos())
+
+func calculate_distance(their_position):
+    return their_position.distance_to(self.get_pos())
+
---
+++ scripts/vendor/entities/moving_entity.gd
@@ -0,0 +1,50 @@
+extends "res://scripts/vendor/entities/entity.gd"
+
+var processing
+
+var velocity
+var movement_vector = Vector3(0, 0, 0)
+
+var position_constraint_positive = Vector3(0, 0, 0)
+var position_constraint_negative = Vector3(0, 0, 0)
+var constrain_position = false
+
+func _init(board, processing).(board):
+    self.processing = processing
+
+func spawn(position):
+    .spawn(position)
+    self.processing.register(self)
+
+func despawn():
+    self.processing.remove(self)
+    .despawn()
+
+func reset_movement():
+    self.movement_vector = Vector3()
+
+func process(delta):
+    self._modify_position(delta)
+    self._constrain_position()
+
+func _modify_position(delta):
+    var motion
+
+    motion = self.movement_vector * self.velocity
+    self.avatar.move_and_slide(self.motion)
+
+func _constrain_position():
+    if not self.constrain_position:
+        return
+
+    var position = self.get_pos()
+
+    position.x = min(position.x, self.position_constraint_positive.x)
+    position.y = min(position.y, self.position_constraint_positive.y)
+    position.z = min(position.z, self.position_constraint_positive.z)
+
+    position.x = max(position.x, self.position_constraint_negative.x)
+    position.y = max(position.y, self.position_constraint_negative.y)
+    position.z = max(position.z, self.position_constraint_negative.z)
+
+    self.set_pos(position)
